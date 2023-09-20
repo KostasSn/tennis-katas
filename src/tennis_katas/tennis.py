dd9b4aa13a6a579c576c70532a26c592e0b773eb
ksyn <ksyn@Kostass-MacBook-Pro.local>
2023-09-20 21:00:20 +0300

    replaced if statements with polymorphism

--- src/tennis_katas/tennis.py
+++ src/tennis_katas/tennis.py
@@ -1,70 +1,7 @@
-from enum import Enum
 from dataclasses import dataclass
 
-
-class FindResult:
-    """_summary_"""
-
-    def get_result(self, player1_points: int, player2_points: int) -> str:
-        if player1_points == player2_points:
-            return self.draw_result()
-
-
-class Draw:
-    def draw_result(self):
-        return {
-            0: "Love-All",
-            1: "Fifteen-All",
-            2: "Thirty-All",
-        }.get(self.Player.points, "Deuce")
-
-
-class Advantage:
-    def adv_result(self):
-        minus_result = self.player1.points - self.player2.points
-        if minus_result == 1:
-            result = "Advantage player1"
-        elif minus_result == -1:
-            result = "Advantage player2"
-        return result
-
-
-class Win:
-    def win_result(self):
-        minus_result = self.player1.points - self.player2.points
-        if minus_result >= 2:
-            result = "Win for player1"
-        else:
-            result = "Win for player2"
-        return result
-
-
-class MiddleScore:
-    def middle_score(self):
-        temp_score = 0
-        result = ""
-        for i in range(1, 3):
-            if i == 1:
-                temp_score = self.player1.points
-            else:
-                result += "-"
-                temp_score = self.player2.points
-            result += {
-                0: "Love",
-                1: "Fifteen",
-                2: "Thirty",
-                3: "Forty",
-            }[temp_score]
-        return result
-
-
-class ScoreSystem(Enum):
-    LOVE = 0
-    FIFTEEN = 1
-    THIRTY = 2
-    FORTY = 3
-    DEUCE = 4
-    ADVANTAGE = 5
+# from abc import ABC, abstractmethod
+# from enum import Enum
 
 
 @dataclass
@@ -74,3 +11,77 @@ class Player:
 
     def score_point(self):
         self.points += 1
+
+
+class Result:
+    """_summary_"""
+
+    def __init__(self, _player1, _player2) -> None:
+        self.player1 = _player1
+        self.player2 = _player2
+
+    def get_result(
+        self,
+    ) -> str:
+        pass
+
+    def result_description(self, score: int = 0):
+        return {
+            0: "Love",
+            1: "Fifteen",
+            2: "Thirty",
+            3: "Forty",
+        }[score]
+
+
+class Draw(Result):
+    def get_result(self):
+        return {
+            0: "Love-All",
+            1: "Fifteen-All",
+            2: "Thirty-All",
+        }.get(self.player1.points, "Deuce")
+
+
+class MiddleScore(Result):
+    def get_result(self):
+        return f"{self.result_description(self.player1.points)}-{self.result_description(self.player2.points)}"
+
+
+class AdvOrWin(Result):
+    def get_result(self):
+        minus_result = self.player1.points - self.player2.points
+        if minus_result == 1:
+            return "Advantage player1"
+        elif minus_result == -1:
+            return "Advantage player2"
+        elif minus_result >= 2:
+            return "Win for player1"
+        else:
+            return "Win for player2"
+
+
+# class Advantage(Result):
+#     def get_result(self):
+#         minus_result = self.player1.points - self.player2.points
+#         if minus_result == 1:
+#             return "Advantage player1"
+#         elif minus_result == -1:
+#             return "Advantage player2"
+
+
+# class Win(Result):
+#     def get_result(self):
+#         minus_result = self.player1.points - self.player2.points
+#         if minus_result >= 2:
+#             return "Win for player1"
+#         else:
+#             return "Win for player2"
+
+# class ScoreSystem(Enum):
+#     LOVE = 0
+#     FIFTEEN = 1
+#     THIRTY = 2
+#     FORTY = 3
+#     DEUCE = 4
+#     ADVANTAGE = 5
--- src/tennis_katas/tennis_game1.py
+++ src/tennis_katas/tennis_game1.py
@@ -1,14 +1,15 @@
-from src.tennis_katas.tennis import Player, ScoreSystem
+from src.tennis_katas.tennis import Player, Result, Draw, MiddleScore, AdvOrWin
 
 
 class TennisGame1:
     def __init__(
         self,
-        player1_name: Player,
-        player2_name: Player,
+        player1_name: str,
+        player2_name: str,
     ):
         self.player1 = Player(player1_name, 0)
         self.player2 = Player(player2_name, 0)
+        self.result = Result(self.player1, self.player2)
 
     def won_point(self, player_name):
         if player_name == self.player1.name:
@@ -18,54 +19,54 @@ class TennisGame1:
 
     def score(self):
         if self.player1.points == self.player2.points:
-            return self.draw_result()
+            return Draw.get_result(self.result)
         elif self.player1.points >= 4 or self.player2.points >= 4:
-            return self.final_result()
+            return AdvOrWin.get_result(self.result)
         else:
-            return self.live_score()
+            return MiddleScore.get_result(self.result)
 
-    def draw_result(self):
-        return {
-            0: "Love-All",
-            1: "Fifteen-All",
-            2: "Thirty-All",
-        }.get(self.player1.points, "Deuce")
+    # def draw_result(self):
+    #     return {
+    #         0: "Love-All",
+    #         1: "Fifteen-All",
+    #         2: "Thirty-All",
+    #     }.get(self.player1.points, "Deuce")
 
-    def final_result(self):
-        minus_result = self.player1.points - self.player2.points
-        if minus_result == 1:
-            return "Advantage player1"
-        elif minus_result == -1:
-            return "Advantage player2"
-        elif minus_result >= 2:
-            return "Win for player1"
-        else:
-            return "Win for player2"
+    # def final_result(self):
+    #     minus_result = self.player1.points - self.player2.points
+    #     if minus_result == 1:
+    #         return "Advantage player1"
+    #     elif minus_result == -1:
+    #         return "Advantage player2"
+    #     elif minus_result >= 2:
+    #         return "Win for player1"
+    #     else:
+    #         return "Win for player2"
 
-    # def live_result(self):
-    #     temp_score = 0
-    #     result = ""
-    #     for i in range(1, 3):
-    #         if i == 1:
-    #             temp_score = self.player1.points
-    #         else:
-    #             result += "-"
-    #             temp_score = self.player2.points
-    #         result += {
-    #             0: "Love",
-    #             1: "Fifteen",
-    #             2: "Thirty",
-    #             3: "Forty",
-    #         }[temp_score]
-    #     return result
+    # # def live_result(self):
+    # #     temp_score = 0
+    # #     result = ""
+    # #     for i in range(1, 3):
+    # #         if i == 1:
+    # #             temp_score = self.player1.points
+    # #         else:
+    # #             result += "-"
+    # #             temp_score = self.player2.points
+    # #         result += {
+    # #             0: "Love",
+    # #             1: "Fifteen",
+    # #             2: "Thirty",
+    # #             3: "Forty",
+    # #         }[temp_score]
+    # #     return result
 
-    def live_score(self):
-        return f"{self.result_description(self.player1.points)}-{self.result_description(self.player2.points)}"
+    # def live_score(self):
+    #     return f"{self.result_description(self.player1.points)}-{self.result_description(self.player2.points)}"
 
-    def result_description(self, score: int = 0):
-        return {
-            0: "Love",
-            1: "Fifteen",
-            2: "Thirty",
-            3: "Forty",
-        }[score]
+    # def result_description(self, score: int = 0):
+    #     return {
+    #         0: "Love",
+    #         1: "Fifteen",
+    #         2: "Thirty",
+    #         3: "Forty",
+    #     }[score]
